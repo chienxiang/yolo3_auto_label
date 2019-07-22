4cd5e4511f725321ad20f3c658417187fa0c9b79
chienxiang <chien.xiang.lai@gmail.com>
2019-07-22 14:01:50 +0800

    convexityDefects

--- opencv/opencv_sh4.py
+++ opencv/opencv_sh4.py
@@ -7,7 +7,7 @@ imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 print ("there are " + str(len(contours)) + " contours")
- 
+
 cnt = contours[0]
 print ("there are " + str(len(cnt)) + " points in contours[0]")
 hull = cv2.convexHull(cnt,returnPoints = False)
